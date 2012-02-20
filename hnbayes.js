d16dce3f0b87c2a76b8cd6bffb829cb1d80772a6
Roger Braun <davinelulinvega@gmail.com>
2012-02-20 23:47:58 +0100

    first

---
+++ hnbayes.js
@@ -0,0 +1,7 @@
+javascript:(function(e,a,g,h,f,c,b,d){if(!(f=e.jQuery)||g>f.fn.jquery||h(f)){c=a.createElement("script");c.type="text/javascript";c.src="http://ajax.googleapis.com/ajax/libs/jquery/"+g+"/jquery.min.js";c.onload=c.onreadystatechange=function(){if(!b&&(!(d=this.readyState)||d=="loaded"||d=="complete")){h((f=e.jQuery).noConflict(1),b=1);f(c).remove()}};a.documentElement.childNodes[0].appendChild(c)}})(window,document,"1.7.1",function($,L){
+// My code
+  $(".title:nth-child(3) a").bind("click", function(event){  
+    alert(event.target.href);}
+    );
+
+});
